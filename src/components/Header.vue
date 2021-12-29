288946118b0c8b63aad9f63e8aec7e13f7064fde
KimEunJeong77 <cheritis@naver.com>
2021-12-29 16:51:56 +0900

    Start project

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+            <RouterLink class="nav-link" active-class="active" to="/">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+            <RouterLink class="nav-link" active-class="active" to="/movie">Movie</RouterLink>
+        </li>
+        <li class="nav-item">
+            <RouterLink class="nav-link" active-class="active" to="/about">About</RouterLink>
+        </li>
+    </ul>    
+</template>
+<script>
+export default{
+    data(){
+        return{
+            navigations:[
+                {name:'Home',href:'/'},
+                {name:'Movie',href:'/movie'},
+                {name:'About',href:'/about'}
+            ]
+        }
+    }
+}
+</script>
